2164cbefcad16ec85d59272b47ef290919c01032
RMitsui <mitsui.r.aa@m.titech.ac.jp>
2020-06-13 22:07:16 +0900

    add make route button

--- app/src/main/java/jp/ac/titech/itpro/sdl/randomwalker/MapsActivity.kt
+++ app/src/main/java/jp/ac/titech/itpro/sdl/randomwalker/MapsActivity.kt
@@ -1,7 +1,13 @@
 package jp.ac.titech.itpro.sdl.randomwalker
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,10 +15,26 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private var tag = MapsActivity::class.java.name
+    private lateinit var fusedLocationProviderClient:FusedLocationProviderClient
     private lateinit var mMap: GoogleMap
+    private var NowLat :Double= 0.0
+    private var NowLng :Double= 0.0
+    private val LatOne = 0.011
+    private val LngOne = 0.009
+    private var Times = 1.0
+    private var rp = DoubleArray(22)
+    private lateinit var gAPIKey :String
+    private var string: String = ""
+    private var peaceful = true
+    private var encodedpl :String = ""
+    companion object{
+        const val RANDOMPOINTS = "RandomWalker.RandomPoints"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,17 +43,48 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        val appInfo = packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
+        val gAPIKey = appInfo.metaData.getString("com.google.android.geo.API_KEY")
+
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+        val locationRequest = LocationRequest().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        }
+        locationRequest.setExpirationDuration(1000L)
+
+        val locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                val location = locationResult?.lastLocation ?: return
+                textView.text = "緯度:${location.latitude},経度:${location.longitude}"
+                NowLat = location.latitude
+                NowLng = location.longitude
+            }
+        }
+
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null)
+
+        bt_makeroute.setOnClickListener {
+            Log.d(tag, "makeLocationRequest")
+            //現在位置の緯度経度を取得
+            val newLocationRequest = LocationRequest().apply {
+                interval = 10000
+                fastestInterval = 5000
+                priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+            }
+            locationRequest.setExpirationDuration(1000L)
+            fusedLocationProviderClient.requestLocationUpdates(
+                newLocationRequest,
+                locationCallback,
+                null
+            )
+        }
+
+
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
